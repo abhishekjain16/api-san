e1cc6672b0d3bc413e46d2e5851d11e8bbf129dc
Abhishek Jain <jainabhishek1610@gmail.com>
2016-10-05 18:07:44 +0530

    Add route file

---
+++ app/assets/javascripts/config/routes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ApiRequestForm from '../components/api_request_form/index';
+import ApiResponse from '../components/api_response/index';
+import { Route, IndexRoute } from 'react-router';
+
+export default (
+  <Route path="/" component={ApiRequestForm}>
+  </Route>
+);
