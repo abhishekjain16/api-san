78adf2449823cf00d640d5a6a4c91f1f0a32fd24
Abhishek Jain <jainabhishek1610@gmail.com>
2016-12-07 23:35:10 +0530

    Fix npm test (#45)

--- app/assets/javascripts/components/routes.js
+++ app/assets/javascripts/components/routes.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
 import ApiRequestForm from './api_request_form/index';
 import EditApiRequest from './api_request_form/edit';
 import ApiResponse from './api_response/index';
-import App from './app'
-import { Route, IndexRoute } from 'react-router';
+import App from './app';
 
 export default (
   <Route path="/" component={App}>
