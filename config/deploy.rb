71f8d92dba93fc89bb3b906dafa0b3a4d3d599f0
Abhishek Jain <jainabhishek1610@gmail.com>
2016-12-08 14:17:59 +0530

    Add task for clearing assets cache (#49)

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,7 +49,7 @@ set :bundle_jobs, 4
 
 set :npm_flags, '--silent --no-progress'
 
-before 'deploy:compile_assets', 'task:exec[tmp:cache:clear]'
+before 'deploy:compile_assets', 'assets:clear_tmp'
 before 'deploy:migrate', 'db:backup'
 after 'deploy:finishing', 'deploy:cleanup'
 after 'deploy:cleanup', 'unicorn:restart'
